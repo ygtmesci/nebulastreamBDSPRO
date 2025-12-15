nebulastream/nebulastream

feat(Null): Added IsNull function for checking if a value is null

nes-physical-operators/include/Functions/BooleanFunctions/IsNullCheckPhysicalFunction.hpp
@@ -1,0 +1,35 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#pragma once
+
+#include <Functions/PhysicalFunction.hpp>
+#include <Nautilus/DataTypes/VarVal.hpp>
+#include <Nautilus/Interface/Record.hpp>
+#include <ExecutionContext.hpp>
+
+namespace NES
+{
+
+/// Checks if the value, returned by the child function, is null
+class IsNullCheckPhysicalFunction final : public PhysicalFunctionConcept
+{
+public:
+    explicit IsNullCheckPhysicalFunction(PhysicalFunction childFunction);
+    [[nodiscard]] VarVal execute(const Record& record, ArenaRef& arena) const override;
+
+private:
+    PhysicalFunction childFunction;
+};
+}

nes-physical-operators/src/Functions/BooleanFunctions/IsNullCheckPhysicalFunction.cpp
@@ -1,0 +1,45 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+#include <Functions/BooleanFunctions/IsNullCheckPhysicalFunction.hpp>
+
+#include <utility>
+#include <Functions/PhysicalFunction.hpp>
+#include <Nautilus/DataTypes/VarVal.hpp>
+#include <Nautilus/Interface/Record.hpp>
+#include <ErrorHandling.hpp>
+#include <ExecutionContext.hpp>
+#include <PhysicalFunctionRegistry.hpp>
+
+namespace NES
+{
+
+VarVal IsNullCheckPhysicalFunction::execute(const Record& record, ArenaRef& arena) const
+{
+    const auto value = childFunction.execute(record, arena);
+    return value.isNull();
+}
+
+IsNullCheckPhysicalFunction::IsNullCheckPhysicalFunction(PhysicalFunction childFunction)
+    : childFunction(std::move(std::move(childFunction)))
+{
+}
+
+PhysicalFunctionRegistryReturnType
+PhysicalFunctionGeneratedRegistrar::RegisterIsNullPhysicalFunction(PhysicalFunctionRegistryArguments physicalFunctionRegistryArguments)
+{
+    PRECONDITION(physicalFunctionRegistryArguments.childFunctions.size() == 1, "IsNull function must have exactly one sub-function");
+    return IsNullCheckPhysicalFunction{physicalFunctionRegistryArguments.childFunctions[0]};
+}
+
+}

ygtmesci/nebulastreamBDSPRO

nes-physical-operators/include/Functions/BooleanFunctions/IsNullCheckPhysicalFunction.hpp
@@ -1,0 +1,35 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#pragma once
+
+#include <Functions/PhysicalFunction.hpp>
+#include <Nautilus/DataTypes/VarVal.hpp>
+#include <Nautilus/Interface/Record.hpp>
+#include <Arena.hpp>
+
+namespace NES
+{
+
+/// Checks if the value, returned by the child function, is null
+class IsNullCheckPhysicalFunction final : public PhysicalFunctionConcept
+{
+public:
+    explicit IsNullCheckPhysicalFunction(PhysicalFunction childFunction);
+    [[nodiscard]] VarVal execute(const Record& record, ArenaRef& arena) const override;
+
+private:
+    PhysicalFunction childFunction;
+};
+}

nes-physical-operators/src/Functions/BooleanFunctions/IsNullCheckPhysicalFunction.cpp
@@ -1,0 +1,45 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+#include <Functions/BooleanFunctions/IsNullCheckPhysicalFunction.hpp>
+
+#include <utility>
+#include <Functions/PhysicalFunction.hpp>
+#include <Nautilus/DataTypes/VarVal.hpp>
+#include <Nautilus/Interface/Record.hpp>
+#include <Arena.hpp>
+#include <ErrorHandling.hpp>
+#include <PhysicalFunctionRegistry.hpp>
+
+namespace NES
+{
+
+VarVal IsNullCheckPhysicalFunction::execute(const Record& record, ArenaRef& arena) const
+{
+    const auto value = childFunction.execute(record, arena);
+    return value.isNull();
+}
+
+IsNullCheckPhysicalFunction::IsNullCheckPhysicalFunction(PhysicalFunction childFunction)
+    : childFunction(std::move(std::move(childFunction)))
+{
+}
+
+PhysicalFunctionRegistryReturnType
+PhysicalFunctionGeneratedRegistrar::RegisterIsNullPhysicalFunction(PhysicalFunctionRegistryArguments physicalFunctionRegistryArguments)
+{
+    PRECONDITION(physicalFunctionRegistryArguments.childFunctions.size() == 1, "IsNull function must have exactly one sub-function");
+    return IsNullCheckPhysicalFunction{physicalFunctionRegistryArguments.childFunctions[0]};
+}
+
+}